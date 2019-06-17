b87ab88e396298dd25c1e7bf32374870948b07ec
Łukasz Dydek <lukaszdydek@gmail.com>
2019-06-17 15:33:25 +0200

    (DOM)

--- main.js
+++ main.js
@@ -12,3 +12,6 @@ const age = 34;
 
 alert(`Nazywam się ${name} i mam ${age} lata.`); /* "``" oznacza literał i jego formatowanie jest odzwierciedlane podczas wyświetlania, np. twardy Enter itp. Literały są polecane zamiast metody z plusami.*/
 
+const pusty = document.querySelector('.about__paragraph--pusty, .about__paragraph--pusty.js');
+console.log(pusty.innerHTML);
+pusty.innerHTML = '<p style="font-size:25px; color:red;">Jestem akapitem, który się pojawia!!!</p>';
